c2679ac8fbc008991f139f2eb3f6176cfce3f11b
Evan Barger <epbarger@gmail.com>
2020-06-09 18:53:58 -0400

    Implement custom clear button

--- script/app.js
+++ script/app.js
@@ -22,6 +22,10 @@ Vue.component('filtering', {
     },
   },
   methods: {
+    clearSearch(){
+      this.searchTerm = "";
+      this.emitFilteredListings();
+    },
     emitFilteredListings() {
       this.$emit(
         'filtered-listings-updated',
@@ -52,7 +56,8 @@ Vue.component('filtering', {
   template: `
     <div class='filtering row'>
       <div class='search col-sm-7'>
-        <input type='search' v-model:value="searchTerm" placeholder="Enter a search term" class="form-control form-control-sm" @keyup="emitFilteredListings"/>
+        <input type='text' aria-label="search" v-model:value="searchTerm" placeholder="Enter a search term" class="form-control form-control-sm" @keyup="emitFilteredListings"/>
+        <button class="clear-btn btn btn-link" v-if="searchTerm.length > 0" @click="clearSearch">✗</button>
         <span class='filter-count'>{{countString}}</span>
       </div>
 
